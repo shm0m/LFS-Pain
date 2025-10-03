213a078d5aae433b1572d5f16b2216605021e2b9
shm0m <shaima.derouich@edu.ece.fr>
2025-10-03 22:56:13 +0200

    feat : correction d'artefacts

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -1,30 +1,30 @@
+#include <stdint.h>
 #include "keyboard.h"
 #include "pic.h"
 #include "vga.h"
-#include <stdint.h>
+#include "shell.h"
 
-static inline uint8_t inb(uint16_t p){
+
+static const char azerty_map[128] = {
+  0, 27, '&', 'é', '"', '\'', '(', '-', 'è', '_', 'ç', 'à', ')', '=', '\b',
+  '\t','a','z','e','r','t','y','u','i','o','p','^','$','\n',0,
+  'q','s','d','f','g','h','j','k','l','m','ù','`',0,'*',
+  'w','x','c','v','b','n',',',';',';',':','!',0,'*',0,' ',
+};
+
+static inline uint8_t inb(uint16_t p) {
     uint8_t v;
     __asm__ volatile("inb %1,%0" : "=a"(v) : "Nd"(p));
     return v;
 }
 
-
-static const char map[128] = {
-  0,27,'1','2','3','4','5','6','7','8','9','0','-','=','\b',
-  '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',0,
-  'a','s','d','f','g','h','j','k','l',';','\'','`',0,'\\',
-  'z','x','c','v','b','n','m',',','.','/',0,'*',0,' ',
-};
-
-
-void irq1_handler(void){
-  uint8_t sc = inb(0x60);
-  if(sc < 128){
-    char c = map[sc];
-    if(c) { char s[2]={c,0}; vga_puts(s); }
-  }
-  pic_eoi(1);
+void irq1_handler(void) {
+    uint8_t scancode = inb(0x60);
+    if (scancode < 128) {
+        char c = azerty_map[scancode];
+        if (c) {
+            shell_on_char(c);
+        }
+    }
+    pic_eoi(1);
 }
-
-
