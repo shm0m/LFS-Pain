187e9399ad9a55a0748362bc79c094b34f6a55e0
shaïma <114345002+shm0m@users.noreply.github.com>
2025-10-04 12:39:11 +0200

    Implement basic IDT and exception handling

--- boot/isr_stubs.asm
+++ boot/isr_stubs.asm
@@ -1,32 +1,93 @@
 BITS 32
-GLOBAL isr_stubs
+
+section .text
+
+GLOBAL isr_stub_table
 GLOBAL idt_load
+
 EXTERN isr_handler
 
-isr_stubs:
+isr_stub_table:
 %assign i 0
 %rep 32
-    extern isr%+i
+    dd isr%+i
 %assign i i+1
 %endrep
 
-%assign i 0
-%rep 32
-isr%+i:
-    push dword i
+%macro ISR_NOERR 1
+isr%1:
     push dword 0
+    push dword %1
     jmp isr_common
-%assign i i+1
-%endrep
+%endmacro
+
+%macro ISR_ERR 1
+isr%1:
+    push dword %1
+    jmp isr_common
+%endmacro
+
+ISR_NOERR 0
+ISR_NOERR 1
+ISR_NOERR 2
+ISR_NOERR 3
+ISR_NOERR 4
+ISR_NOERR 5
+ISR_NOERR 6
+ISR_NOERR 7
+ISR_ERR   8
+ISR_NOERR 9
+ISR_ERR   10
+ISR_ERR   11
+ISR_ERR   12
+ISR_ERR   13
+ISR_ERR   14
+ISR_NOERR 15
+ISR_NOERR 16
+ISR_ERR   17
+ISR_NOERR 18
+ISR_NOERR 19
+ISR_NOERR 20
+ISR_NOERR 21
+ISR_NOERR 22
+ISR_NOERR 23
+ISR_NOERR 24
+ISR_NOERR 25
+ISR_NOERR 26
+ISR_NOERR 27
+ISR_NOERR 28
+ISR_NOERR 29
+ISR_NOERR 30
+ISR_NOERR 31
 
 isr_common:
     pusha
+    push ds
+    push es
+    push fs
+    push gs
+
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov eax, esp
+    push eax
     call isr_handler
+    add esp, 4
+
+    pop gs
+    pop fs
+    pop es
+    pop ds
     popa
+
     add esp, 8
     iretd
 
 idt_load:
-    mov eax, [esp+4]
+    mov eax, [esp + 4]
     lidt [eax]
     ret
--- kernel/idt.h
+++ kernel/idt.h
@@ -1,16 +1,21 @@
 #pragma once
+
 #include <stdint.h>
 
+#define IDT_MAX_ENTRIES 256
+
 typedef struct {
-  uint16_t base_lo, sel;
-  uint8_t  always0, flags;
-  uint16_t base_hi;
+    uint16_t base_low;
+    uint16_t selector;
+    uint8_t  zero;
+    uint8_t  type_attr;
+    uint16_t base_high;
 } __attribute__((packed)) idt_entry_t;
 
 typedef struct {
-  uint16_t limit;
-  uint32_t base;
+    uint16_t limit;
+    uint32_t base;
 } __attribute__((packed)) idt_ptr_t;
 
-void idt_set_gate(int n, uint32_t base, uint16_t sel, uint8_t flags);
+void idt_set_gate(uint8_t vector, uint32_t base, uint16_t selector, uint8_t type_attr);
 void idt_init(void);
